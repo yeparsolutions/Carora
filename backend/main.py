abdc69aff7224012c556e639d457486a5e70d910
Yepar Solutions <yeparsolutions@gmail.com>
2026-02-21 15:11:09 -0300

    Sincronización automática Carora — 2026-02-21 15:11:09

--- backend/main.py
+++ backend/main.py
@@ -35,8 +35,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "http://localhost:5500/",
+        "http://127.0.0.1:5500/",
         "http://localhost:8000",
         "null",
+        "*",
     ],
     allow_credentials = True,
     allow_methods     = ["*"],
